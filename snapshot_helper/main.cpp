7ca1e782562449fe84c01ee042735128dcbdcb8e
jftkcs <120062444+jftkcs@users.noreply.github.com>
2025-04-25 19:04:03 -0700

    Yanked zfs chown fix from PR #53 (40c08dd)

--- snapshot_helper/main.cpp
+++ snapshot_helper/main.cpp
@@ -347,7 +347,9 @@ bool create_subvolume(int mode, std::string subvolume_folder)
 	{
 		zfs_elevate();
 		int rc=exec_wait(find_zfs_cmd(), true, "create", "-p", subvolume_folder.c_str(), NULL);
-		chown_dir(subvolume_folder);
+		std::string subvolume_path;
+		exec_wait(find_zfs_cmd(), subvolume_path, "get", "-H", "-o", "value", "mountpoint", subvolume_folder.c_str(), NULL);
+		chown_dir(trim(subvolume_path));
 		return rc==0;
 	}
 	return false;
@@ -390,7 +392,9 @@ bool create_snapshot(int mode, std::string snapshot_src, std::string snapshot_ds
 	{
 		zfs_elevate();
 		int rc=exec_wait(find_zfs_cmd(), true, "clone", (snapshot_src+"@ro").c_str(), snapshot_dst.c_str(), NULL);
-		chown_dir(snapshot_dst);
+		std::string subvolume_path;
+		exec_wait(find_zfs_cmd(), subvolume_path, "get", "-H", "-o", "value", "mountpoint", subvolume_folder.c_str(), NULL);
+		chown_dir(trim(subvolume_path));
 		return rc==0;
 	}
 	return false;
